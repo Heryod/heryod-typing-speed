2a826d7d5dbc4188c675ff65568bdab041aff5a0
Heryod <heryod.pardej@gmail.com>
2025-03-20 09:04:09 +0000

    Replace React logo with a keyboard icon in the navigation component and update related assets and styles

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,13 @@
 import "./Nav.css";
+import KeyboardIcon from "../assets/keyboard.svg";
 
 export function Nav() {
   return (
     <nav>
       <ul>
+        <li>
+          <img className="logo-icon" src={KeyboardIcon} alt="KeyboardIcon" />
+        </li>
         <li>
           <a href="/">Home</a>
         </li>
